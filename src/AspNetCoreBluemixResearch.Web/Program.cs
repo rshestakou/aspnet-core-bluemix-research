84ee0f7cd9453d1e61d821749bc797afb874cae3
Raman Shestakou <rshestakou@gomel.iba.by>
2016-11-04 10:17:49 +0300

    Replaced Starter Pack with standard MVC template project

---
+++ src/AspNetCoreBluemixResearch.Web/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace AspNetCoreBluemixResearch.Web
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var config = new ConfigurationBuilder()
+        .AddCommandLine(args)
+        .Build();
+
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseIISIntegration()
+        .UseConfiguration(config)
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
