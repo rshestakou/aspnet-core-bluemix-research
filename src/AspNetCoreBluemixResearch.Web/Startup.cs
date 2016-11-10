7d328d7d3e7e91e53bf3f2a169a1be0ee98c98ac
Raman Shestakou <rshestakou@gomel.iba.by>
2016-11-10 16:44:20 +0300

    Added comments about switching provider

--- src/AspNetCoreBluemixResearch.Web/Startup.cs
+++ src/AspNetCoreBluemixResearch.Web/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using AspNetCoreBluemixResearch.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,10 +25,12 @@ namespace AspNetCoreBluemixResearch.Web
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			string connection = Configuration.GetConnectionString("MySQLConnection");
+			//string connection = Configuration.GetConnectionString("MySQLConnection");
+			string connection = Configuration.GetConnectionString("PostgreSQLConnection");
 
 			services.AddDbContext<MobileContext>(options =>
-				options.UseMySQL(connection));
+				//options.UseMySQL(connection));
+				options.UseNpgsql(connection));
 
 			// Add framework services.
 			services.AddMvc();
